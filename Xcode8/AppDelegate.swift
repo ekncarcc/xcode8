1c8abc8816f672da4b40fb1cca3dd075dcd6e536
Robert Johannesson <rj9@Roberts-MacBook-Pro.local>
2017-01-08 15:37:45 -0500

    Initial Commit

---
+++ Xcode8/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8
+//
+//  Created by Robert Johannesson on 1/8/17.
+//  Copyright © 2017 Robert Johannesson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8
+//
+//  Created by Robert Johannesson on 1/8/17.
+//  Copyright © 2017 Robert Johannesson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
